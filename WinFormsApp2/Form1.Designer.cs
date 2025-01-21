709940a96a236a0f2cd4ed2fcbe66e5bece51100
Ittiphon2005 <ittiphon.j@kkumail.com>
2025-01-21 11:28:29 +0700

    Add files via upload

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace WinFormsApp2
+{
+    partial class t
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            coffeeP = new TextBox();
+            tbTotal = new TextBox();
+            tbChange = new TextBox();
+            grP = new TextBox();
+            tb1000 = new TextBox();
+            tbCash = new TextBox();
+            tb100 = new TextBox();
+            tb500 = new TextBox();
+            tb20 = new TextBox();
+            tb50 = new TextBox();
+            tb5 = new TextBox();
+            tb10 = new TextBox();
+            tb1 = new TextBox();
+            grQ = new TextBox();
+            coffeeQ = new TextBox();
+            checkc = new CheckBox();
+            checkg = new CheckBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(442, 64);
+            button1.Name = "button1";
+            button1.Size = new Size(98, 303);
+            button1.TabIndex = 0;
+            button1.Text = "Check out";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // coffeeP
+            // 
+            coffeeP.Location = new Point(174, 74);
+            coffeeP.Name = "coffeeP";
+            coffeeP.Size = new Size(100, 23);
+            coffeeP.TabIndex = 1;
+            // 
+            // tbTotal
+            // 
+            tbTotal.Location = new Point(629, 57);
+            tbTotal.Name = "tbTotal";
+            tbTotal.Size = new Size(100, 23);
+            tbTotal.TabIndex = 2;
+            // 
+            // tbChange
+            // 
+            tbChange.Location = new Point(629, 86);
+            tbChange.Name = "tbChange";
+            tbChange.Size = new Size(100, 23);
+            tbChange.TabIndex = 4;
+            // 
+            // grP
+            // 
+            grP.Location = new Point(174, 103);
+            grP.Name = "grP";
+            grP.Size = new Size(100, 23);
+            grP.TabIndex = 3;
+            // 
+            // tb1000
+            // 
+            tb1000.Location = new Point(629, 144);
+            tb1000.Name = "tb1000";
+            tb1000.Size = new Size(100, 23);
+            tb1000.TabIndex = 6;
+            // 
+            // tbCash
+            // 
+            tbCash.Location = new Point(629, 115);
+            tbCash.Name = "tbCash";
+            tbCash.Size = new Size(100, 23);
+            tbCash.TabIndex = 5;
+            // 
+            // tb100
+            // 
+            tb100.Location = new Point(629, 202);
+            tb100.Name = "tb100";
+            tb100.Size = new Size(100, 23);
+            tb100.TabIndex = 8;
+            // 
+            // tb500
+            // 
+            tb500.Location = new Point(629, 173);
+            tb500.Name = "tb500";
+            tb500.Size = new Size(100, 23);
+            tb500.TabIndex = 7;
+            // 
+            // tb20
+            // 
+            tb20.Location = new Point(629, 260);
+            tb20.Name = "tb20";
+            tb20.Size = new Size(100, 23);
+            tb20.TabIndex = 10;
+            // 
+            // tb50
+            // 
+            tb50.Location = new Point(629, 231);
+            tb50.Name = "tb50";
+            tb50.Size = new Size(100, 23);
+            tb50.TabIndex = 9;
+            // 
+            // tb5
+            // 
+            tb5.Location = new Point(629, 318);
+            tb5.Name = "tb5";
+            tb5.Size = new Size(100, 23);
+            tb5.TabIndex = 12;
+            // 
+            // tb10
+            // 
+            tb10.Location = new Point(629, 289);
+            tb10.Name = "tb10";
+            tb10.Size = new Size(100, 23);
+            tb10.TabIndex = 11;
+            // 
+            // tb1
+            // 
+            tb1.Location = new Point(629, 347);
+            tb1.Name = "tb1";
+            tb1.Size = new Size(100, 23);
+            tb1.TabIndex = 13;
+            // 
+            // grQ
+            // 
+            grQ.Location = new Point(295, 103);
+            grQ.Name = "grQ";
+            grQ.Size = new Size(100, 23);
+            grQ.TabIndex = 15;
+            // 
+            // coffeeQ
+            // 
+            coffeeQ.Location = new Point(295, 74);
+            coffeeQ.Name = "coffeeQ";
+            coffeeQ.Size = new Size(100, 23);
+            coffeeQ.TabIndex = 14;
+            // 
+            // checkc
+            // 
+            checkc.AutoSize = true;
+            checkc.Location = new Point(85, 76);
+            checkc.Name = "checkc";
+            checkc.Size = new Size(61, 19);
+            checkc.TabIndex = 16;
+            checkc.Text = "Coffee";
+            checkc.UseVisualStyleBackColor = true;
+            checkc.CheckedChanged += checkc_CheckedChanged;
+            // 
+            // checkg
+            // 
+            checkg.AutoSize = true;
+            checkg.Location = new Point(85, 105);
+            checkg.Name = "checkg";
+            checkg.Size = new Size(77, 19);
+            checkg.TabIndex = 17;
+            checkg.Text = "Green Tea";
+            checkg.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(591, 60);
+            label1.Name = "label1";
+            label1.Size = new Size(32, 15);
+            label1.TabIndex = 18;
+            label1.Text = "Total";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(591, 152);
+            label2.Name = "label2";
+            label2.Size = new Size(31, 15);
+            label2.TabIndex = 19;
+            label2.Text = "1000";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(588, 123);
+            label3.Name = "label3";
+            label3.Size = new Size(33, 15);
+            label3.TabIndex = 20;
+            label3.Text = "Cash";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(602, 234);
+            label4.Name = "label4";
+            label4.Size = new Size(19, 15);
+            label4.TabIndex = 21;
+            label4.Text = "50";
+            label4.Click += label4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(596, 208);
+            label5.Name = "label5";
+            label5.Size = new Size(25, 15);
+            label5.TabIndex = 22;
+            label5.Text = "100";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(596, 181);
+            label6.Name = "label6";
+            label6.Size = new Size(25, 15);
+            label6.TabIndex = 23;
+            label6.Text = "500";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(602, 263);
+            label7.Name = "label7";
+            label7.Size = new Size(19, 15);
+            label7.TabIndex = 24;
+            label7.Text = "20";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(608, 321);
+            label8.Name = "label8";
+            label8.Size = new Size(13, 15);
+            label8.TabIndex = 25;
+            label8.Text = "5";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(602, 292);
+            label9.Name = "label9";
+            label9.Size = new Size(19, 15);
+            label9.TabIndex = 26;
+            label9.Text = "10";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(575, 89);
+            label10.Name = "label10";
+            label10.Size = new Size(48, 15);
+            label10.TabIndex = 27;
+            label10.Text = "Change";
+            label10.Click += label10_Click;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(608, 350);
+            label11.Name = "label11";
+            label11.Size = new Size(13, 15);
+            label11.TabIndex = 28;
+            label11.Text = "1";
+            // 
+            // t
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(checkg);
+            Controls.Add(checkc);
+            Controls.Add(grQ);
+            Controls.Add(coffeeQ);
+            Controls.Add(tb1);
+            Controls.Add(tb5);
+            Controls.Add(tb10);
+            Controls.Add(tb20);
+            Controls.Add(tb50);
+            Controls.Add(tb100);
+            Controls.Add(tb500);
+            Controls.Add(tb1000);
+            Controls.Add(tbCash);
+            Controls.Add(tbChange);
+            Controls.Add(grP);
+            Controls.Add(tbTotal);
+            Controls.Add(coffeeP);
+            Controls.Add(button1);
+            Name = "t";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox coffeeP;
+        private TextBox tbTotal;
+        private TextBox tbChange;
+        private TextBox grP;
+        private TextBox tb1000;
+        private TextBox tbCash;
+        private TextBox tb100;
+        private TextBox tb500;
+        private TextBox tb20;
+        private TextBox tb50;
+        private TextBox tb5;
+        private TextBox tb10;
+        private TextBox tb1;
+        private TextBox grQ;
+        private TextBox coffeeQ;
+        private CheckBox checkc;
+        private CheckBox checkg;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+    }
+}
---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,95 @@
+﻿namespace WinFormsApp2
+{
+    public partial class t : Form
+    {
+        public t()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double dCash = double.Parse(tbCash.Text);
+
+                double dBeverageTotal = 0;
+
+                dBeverageTotal += GetItemTotal(coffeeP.Text, coffeeQ.Text);
+                dBeverageTotal += GetItemTotal(grP.Text, grQ.Text);
+
+                double dGrandTotal = dBeverageTotal;
+
+                if (dCash < dGrandTotal)
+                {
+                    MessageBox.Show("เงินสดไม่เพียงพอ", "ข้อผิดพลาด", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                double dChange = dCash - dGrandTotal;
+
+                tbTotal.Text = dGrandTotal.ToString("F2");
+                tbChange.Text = dChange.ToString("F2");
+
+                CalculateChangeDenominations(dChange);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("กรุณากรอกข้อมูลตัวเลขให้ถูกต้อง", "ข้อผิดพลาด", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private double GetItemTotal(string priceText, string quantityText)
+        {
+            double price = 0, quantity = 0;
+            try
+            {
+                price = double.Parse(priceText);
+                quantity = double.Parse(quantityText);
+            }
+            catch (Exception)
+            {
+                price = 0;
+                quantity = 0;
+            }
+            return price * quantity;
+        }
+
+        private void CalculateChangeDenominations(double change)
+        {
+            double[] denominations = { 1000, 500, 100, 50, 20, 10, 5, 1 };
+            int[] changeCount = new int[denominations.Length];
+            double remainChange = change;
+
+            for (int i = 0; i < denominations.Length; i++)
+            {
+                changeCount[i] = (int)(remainChange / denominations[i]);
+                remainChange %= denominations[i];
+            }
+
+            tb1000.Text = changeCount[0].ToString();
+            tb500.Text = changeCount[1].ToString();
+            tb100.Text = changeCount[2].ToString();
+            tb50.Text = changeCount[3].ToString();
+            tb20.Text = changeCount[4].ToString();
+            tb10.Text = changeCount[5].ToString();
+            tb5.Text = changeCount[6].ToString();
+            tb1.Text = changeCount[7].ToString();
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkc_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+}
+    }
